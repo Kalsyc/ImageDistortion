c4a00bbb8d9f529019b2d830c68a2ff425c83f47
Darren Sim <klassiclegend@gmail.com>
2021-05-10 16:24:45 +0800

    Test svg filters for displacement mapping

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 //Images
 var mainImg = new Image();
 var badMainImg = new Image();
+var displacementImg = new Image();
+var displaceContext;
+var displaceData;
 mainImg.src = "./images/img_midground_good.png";
 badMainImg.src = "./images/img_midground_bad.jpg";
+displacementImg.src = "./images/displace.png";
 
 //Global Variables
 var touchInput;
 var mainCanvas;
 var zoomCanvas;
+var displaceCanvas;
 var offset = document.documentElement.clientHeight / 2;
 var radius = 170;
 var zoom = offset / radius /  1.3  * 0.9;
@@ -15,10 +20,15 @@ var zoom = offset / radius /  1.3  * 0.9;
 function initialize() {
   mainCanvas = document.getElementById("main-canvas");
   zoomCanvas = document.getElementById("zoom-canvas");
+  displaceCanvas = document.getElementById("displace-canvas");
+  displaceContext = displaceCanvas.getContext('2d');
+  displaceData = displaceContext.getImageData(0, 0, displacementImg.width, displacementImg.height);
   resizeCanvas(mainCanvas);
   resizeCanvas(zoomCanvas);
   scaleToFillImage(mainCanvas, mainImg);
   scaleToFillImage(zoomCanvas, badMainImg);
+  const zoomContext = zoomCanvas.getContext('2d');
+  zoomContext.filter = "url(#noise)";
 }
 
 function resizeCanvas(canvas) {
@@ -31,12 +41,19 @@ function scaleToFillImage(canvas, img) {
   const scale = Math.max(canvas.width / img.width, canvas.height / img.height);
   const x = (canvas.width / 2) - (img.width / 2) * scale;
   const y = (canvas.height / 2) - (img.height / 2) * scale;
+  
   context.drawImage(img, x, y, img.width * scale, img.height * scale);
 }
 
 function getGesturePosition(event) {
   if (event.targetTouches) {
     touchInput = simulateTouch(event.targetTouches);
+    return true;
+  } else if (event.clientX) {
+    touchInput = simulateTouch([{
+      clientX: event.clientX,
+      clientY: event.clientY
+    }])
     return true;
   }
   return false;
@@ -62,21 +79,36 @@ function handleGestureStart(event) {
 function handleGestureMove(event) {
   event.preventDefault();
   if (getGesturePosition(event)) {
-    drawCircle();
+    drawCircle(true);
   }
 }
 
-function drawCircle() {
+function drawCircle(bool) {
   if (touchInput !== null && typeof(touchInput) !== "undefined") {
     const zoomContext = zoomCanvas.getContext('2d');
+    const mainContext = mainCanvas.getContext('2d');
     zoomContext.clearRect(0, 0, zoomCanvas.width, zoomCanvas.height);
-          zoomContext.setTransform(zoom, 
-            0, 
-            0, 
-            zoom, 
-            (zoomCanvas.width / 2 - touchInput.center.x) * zoom - zoomCanvas.width / 2 * (zoom - 1), 
-            (1 / 1.3 * zoomCanvas.height / 2 - touchInput.center.y + offset) * zoom - 1 / 1.3 * zoomCanvas.height / 2 * (zoom - 1) - (0.3/ 1.3 * zoomCanvas.height / 2 - 80)* zoom);
-          scaleToFillImage(zoomCanvas, mainImg);
+    zoomContext.setTransform(zoom, 
+      0, 
+      0, 
+      zoom,
+      (zoomCanvas.width / 2 - touchInput.center.x) * zoom - zoomCanvas.width / 2 * (zoom - 1), 
+      (1 / 1.3 * zoomCanvas.height / 2 - touchInput.center.y + offset) * zoom - 1 / 1.3 * zoomCanvas.height / 2 * (zoom - 1) - (0.3/ 1.3 * zoomCanvas.height / 2 - 80)* zoom);
+      //zoomContext.filter = "url(#noise)";
+      //mainContext.filter = "url(#noise)";
+      scaleToFillImage(zoomCanvas, mainImg);
+      zoomContext.filter = "url(#noise)";
+      //mainContext.filter = "url(#noise)";
+    
+    //const imageData = zoomContext.getImageData(0, 0, mainImg.width, mainImg.height);
+    //var filtered = ImageFilters.DisplacementMapFilter(imageData, displaceData);
+    //var filtered = ImageFilters.GrayScale(imageData);
+    //zoomContext.putImageData(filtered, 0, 0);
+    
+    //const filteredImg = new Image();
+    //filteredImg.src = zoomCanvas.toDataURL();
+    //console.log(filteredImg);
+    
   }
 }
 
@@ -84,4 +116,6 @@ window.onload = () => {
   initialize();
   mainCanvas.addEventListener('touchstart', this.handleGestureStart, true);
   mainCanvas.addEventListener('touchmove', this.handleGestureMove, true);
+  mainCanvas.addEventListener('mousedown', this.handleGestureStart, true);
+  mainCanvas.addEventListener('mousemove', this.handleGestureMove, true);
 }
--- src/test.js
+++ src/test.js
@@ -1,20 +1,41 @@
 var ring;
 var displace;
+var displace2;
+var bg2;
+var container2;
+var container;
+var viewport;
 
 function initialize() {
   const app = new PIXI.Application();
   document.body.appendChild(app.view);
   app.stage.interactive = true;
-  const container = new PIXI.Container();
+  container = new PIXI.Container();
+  container2 = new PIXI.Container();
+  viewport = new pixi_viewport.Viewport({
+    screenWidth: 200,
+    screenHeight: 200,
+    worldWidth: 200,
+    worldHeight: 200,
+    interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+  });
+
+  viewport.clampZoom({
+    minWidth: 200,
+    minHeight: 200,
+    maxWidth: 200,
+    maxHeight: 200,
+    minScale: 0.5,
+    minScale: 0.5
+  });
+
   app.stage.addChild(container);
   
+  app.stage.addChild(container2);
+  //container2.addChild(viewport);
+  //app.stage.addChild(viewport);
+  
   const padding = 100;
-  const bounds = new PIXI.Rectangle(
-    -padding,
-    -padding,
-    app.screen.width + padding * 2,
-    app.screen.height + padding * 2,
-  );
 
   app.loader.add([{
     name: 'bg',
@@ -33,9 +54,17 @@ function initialize() {
   }
   ]).load(() => {
     const bg = new PIXI.Sprite(app.loader.resources.bg.texture);
-    bg.width = app.screen.width;
-    bg.height = app.screen.height;
+    bg2 = new PIXI.Sprite(app.loader.resources.bg.texture);
+    bg.width = app.screen.width/2;
+    bg.height = app.screen.height/2;
+    bg2.width = app.screen.width/2;
+    bg2.height = app.screen.height/2;
     container.addChild(bg);
+    
+    //viewport.addChild(bg2);
+    //viewport.position.y = bg.height;
+    container2.addChild(bg2);
+    container2.position.y = bg.height;
 
     ring = new PIXI.Sprite(app.loader.resources.ring.texture);
     ring.anchor.set(0.5);
@@ -43,12 +72,17 @@ function initialize() {
     container.addChild(ring);
 
     displace = new PIXI.Sprite(app.loader.resources.filter.texture)
+    displace2 = new PIXI.Sprite(app.loader.resources.filter.texture)
     const displacementFilter = new PIXI.filters.DisplacementFilter(displace);
+    const displacementFilter2 = new PIXI.filters.DisplacementFilter(displace2);
     container.addChild(displace);
     container.filters = [displacementFilter];
+    container2.addChild(displace2);
+    container2.filters = [displacementFilter2];
     displacementFilter.scale.x = 110;
     displacementFilter.scale.y = 110;
     displace.anchor.set(0.5);
+    viewport.drag().pinch();
 
     app.stage
     .on('mousemove', onPointerMove)
@@ -60,6 +94,32 @@ function onPointerMove(eventData) {
   ring.visible = true;
   displace.position.set(eventData.data.global.x, eventData.data.global.y);
   ring.position.copyFrom(displace.position);
+  var offset = document.documentElement.clientHeight / 2;
+  var radius = 170;
+  var zoom = offset / radius /  1.3  * 0.9;
+  displace2.position.set(eventData.data.global.x, eventData.data.global.y);
+  var mat = new PIXI.Matrix();
+  mat.scale(2,2);
+  mat.translate(0.5, 0.5);
+  bg2.scale = new PIXI.Point(2,2);
+  bg2.position.copyFrom(displace2.position);
+
+  //viewport.drag();
+  //viewport.fit(false, 200, 200);
+  //bg2.setTransform(zoom, 0, 0, zoom, 0, 0, 0, eventData.data.global.x, eventData.data.global.y);
+  /*
+  viewport.setTransform(
+    (container.width / 2 - eventData.data.global.x) * zoom - container.width / 2 * (zoom - 1), 
+    (1 / 1.3 * container.height / 2 - eventData.data.global.y + offset) * zoom - 1 / 1.3 * container.height / 2 * (zoom - 1) - (0.3/ 1.3 * container.height / 2 - 80)* zoom,
+    zoom,
+    zoom,
+    0,
+    0,
+    0,
+    0.5,
+    0.5
+  )
+  */
 }
 
 window.onload = () => {
